0c44fa6e8bb4692aeabe3e58d09c513b52692434
Ryan <ryanslevin@gmail.com>
2019-05-13 19:27:56 -0700

    Converted ProductController from MVC to REST API.

--- src/main/java/com/rs/webscraper/controller/ProductController.java
+++ src/main/java/com/rs/webscraper/controller/ProductController.java
@@ -3,17 +3,18 @@ package com.rs.webscraper.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.rs.webscraper.entity.PriceHistory;
 import com.rs.webscraper.entity.Product;
 import com.rs.webscraper.scraper.ScraperController;
 import com.rs.webscraper.service.ProductService;
 
-@Controller
+@RestController
 public class ProductController {
 
 	@Autowired
@@ -22,85 +23,77 @@ public class ProductController {
 	@Autowired
 	ScraperController scraperController;
 	
-	@GetMapping("/list")
-	public String getProductList(Model theModel) {
-		
-		//Commenting out to test @Scheduled scraping
-		/*
-		scraperController.scrapeProducts();
-		*/
+	@RequestMapping("/products")
+	public List<Product> getProductList(Model theModel) {
 		
 		//Create a list object and call getProducts method on service
 		List<Product> theProducts = productService.getProducts();
 		
-		//add list object to model
-		theModel.addAttribute("theProducts", theProducts);
-		
-		//return the thymeleaf page
-		return "product-list";
+		//return the products
+		return theProducts;
 	}
 	
 	
-	@GetMapping("/list/{productId}")
-	public String getProduct(@PathVariable int productId, Model theModel) {
+	@RequestMapping("/product/{productId}")
+	public Product getProduct(@PathVariable int productId, Model theModel) {
 		
 		//call method to get product from service by id
 		Product theProduct = productService.getProduct(productId);
 		
-		//add product to model
-		theModel.addAttribute("theProduct", theProduct);
+		//return the thymeleaf page
+		return theProduct;
+	}
+	
+	@RequestMapping("/pricehistory/{productId}")
+	public List<PriceHistory> getPriceHistory(@PathVariable int productId, Model theModel) {
 		
 		//call method to get full PriceHistory objects for Product
 		List<PriceHistory> thePriceHistory = productService.getPriceHistory(productId);
 
-		//add PriceHistory to the model
-		theModel.addAttribute("thePriceHistory", thePriceHistory);
-
 		//call method to get current pricing info
 		List<PriceHistory> currentPrices = productService.getCurrentPrices(productId);
-		
-		//add current pricing info to model
-		theModel.addAttribute("currentPrices", currentPrices);
-		
+
 		//return the thymeleaf page
-		return "product";
-	}
+		return thePriceHistory;
+	}	
 	
-	@GetMapping("/brand/{brand}")
-	public String getBrandProducts(@PathVariable String brand, Model theModel) {
+	@RequestMapping("/currentprices/{productId}")
+	public List<PriceHistory> getCurrentPrices(@PathVariable int productId, Model theModel) {
+		
+		//call method to get current pricing info
+		List<PriceHistory> currentPrices = productService.getCurrentPrices(productId);
+
+		//return the thymeleaf page
+		return currentPrices;
+	}		
+	
+	
+	@RequestMapping("/brand/{brand}")
+	public List<Product> getBrandProducts(@PathVariable String brand, Model theModel) {
 		
 		//call method to get the products from service by brand
 		List<Product> theProducts = productService.getBrandProducts(brand);
 		
-		//add products to model
-		theModel.addAttribute("theProducts", theProducts);
-		
-		return "product-list";
+		return theProducts;
 	}
 	
 	
 	@GetMapping("/category/{category}")
-	public String getCategoryProducts(@PathVariable String category, Model theModel) {
+	public List<Product> getCategoryProducts(@PathVariable String category, Model theModel) {
 		
 		//call method to get the products from service by category
 		List<Product> theProducts = productService.getCategoryProducts(category);
 		
-		//add products to model
-		theModel.addAttribute("theProducts", theProducts);
-		
-		return "product-list";
+		return theProducts;
 	}
 	
 	@GetMapping("/subcategory/{subCategory}")
-	public String getSubCategoryProducts(@PathVariable String subCategory, Model theModel) {
+	public List<Product> getSubCategoryProducts(@PathVariable String subCategory, Model theModel) {
 		
 		//call method to get the products from service by category
 		List<Product> theProducts = productService.getSubCategoryProducts(subCategory);
-		
-		//add products to model
-		theModel.addAttribute("theProducts", theProducts);
-		
-		return "product-list";
+
+		return theProducts;
 	}
 	
 	
